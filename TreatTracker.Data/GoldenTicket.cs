7f433dfd02da3aac5fa4830d0532295b83b228fd
tylerklink75 <66278928+tylerklink75@users.noreply.github.com>
2020-07-31 10:48:14 -0400

    adding golden ticket and room

--- TreatTracker.Data/GoldenTicket.cs
+++ TreatTracker.Data/GoldenTicket.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,10 @@ namespace TreatTracker.Data
 {
     public class GoldenTicket
     {
+        [Key]
+        public int TicketId { get; set; }
+        [ForeignKey(nameof(CandyId))]
+        public virtual Candy Candy{ get; set; }
+
     }
 }
--- TreatTracker.Data/Room.cs
+++ TreatTracker.Data/Room.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,13 @@ namespace TreatTracker.Data
 {
     public class Room
     {
+        [Key]
+        public int RoomId { get; set; }
+        [Required]
+        public string Theme { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [ForeignKey(nameof(FactoryId))]
+        public virtual Factory Factory { get; set; } 
     }
 }
