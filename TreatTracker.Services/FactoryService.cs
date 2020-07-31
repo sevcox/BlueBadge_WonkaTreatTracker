3e9b060a53d062e895b56efbec3f6f412fb455d1
SperZ <ZachSperka@gmail.com>
2020-07-31 08:51:31 -0400

    after some services built

---
+++ TreatTracker.Services/FactoryService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TreatTracker.Data;
+using TreatTracker.Models;
+
+namespace TreatTracker.Services
+{
+    public class FactoryService
+    {
+        private readonly Guid _userId;
+        public FactoryService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public IEnumerable<FactoryListItem> GetStores()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Factories
+                    .Select(
+                        e =>
+                    new FactoryListItem
+                    {
+                        StoreId = e.StoreId,
+                        LocationName = e.LocationName
+                    }
+
+                    );
+
+                return query.ToArray();
+            }
+        }
+
+        public FactoryDetail GetStoreById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Factories
+                    .Single(e => e.StoreId == id);
+                return
+                new FactoryDetail
+                {
+                    FactoryId = entity.FactoryId,
+                    LocationName = entity.LocationName,
+                    Address = entity.Address,
+                    PhoneNumber = entity.PhoneNumber,
+                    Manager = entity.Manager
+                };
+
+            }
+        }
+    }
+}
---
+++ TreatTracker.Services/StoreService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using TreatTracker.Data;
+using TreatTracker.Models;
+
+namespace TreatTracker.Services
+{
+    public class StoreService
+    {
+        private readonly Guid _userId;
+        public StoreService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public IEnumerable<StoreListItem> GetStores()
+        {
+            using(var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Stores
+                    .Select(
+                        e =>
+                    new StoreListItem
+                    {
+                        StoreId = e.StoreId,
+                        LocationName = e.LocationName
+                    }
+
+                    );
+
+                return query.ToArray();
+            }
+        }
+
+        public StoreDetail GetStoreById(int id)
+        {
+            using(var ctx = new ApplicationDbContext)
+            {
+                var entity =
+                    ctx
+                    .Stores
+                    .Single(e => e.StoreId == id);
+                    return
+                    new StoreDetail 
+                    {
+                        StoreId = entity.StoreId,
+                        LocationName = entity.LocationName,
+                        Address = entity.Address,
+                        PhoneNumber = entity.PhoneNumber
+                    };
+
+            }
+        }
+
+    }
+}
