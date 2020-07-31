1b192bb123f4dcf44c407ae88495af14f47f4765
SperZ <ZachSperka@gmail.com>
2020-07-31 09:16:54 -0400

    updated FactoryDetail

--- TreatTracker.Models/FactoryDetail.cs
+++ TreatTracker.Models/FactoryDetail.cs
@@ -14,5 +14,6 @@ namespace TreatTracker.Models
         public string LocationName { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
+        public string Manager { get; set; }
     }
 }
