3f9bfd5520b6d7591e69aca22c981a767700a9b6
Severa <severa.cox@gmail.com>
2020-07-30 22:52:34 -0400

    added treat,candy,and drik class

--- TreatTracker.Data/IdentityModels.cs
+++ TreatTracker.Data/IdentityModels.cs
@@ -1,4 +1,8 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -28,6 +32,39 @@ namespace TreatTracker.Data
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+        //Add DbSetsHere
+        public override int SaveChanges()
+        {
+            AddTimestamps();
+            return base.SaveChanges();
+        }
+
+        public override async Task<int> SaveChangesAsync()
+        {
+            AddTimestamps();
+            return await base.SaveChangesAsync();
+        }
+
+        private void AddTimestamps()
+        {
+            var entities = ChangeTracker.Entries().Where(x => x.Entity is BaseEntity && (x.State == EntityState.Added || x.State == EntityState.Modified));
+
+            var currentUsername = !string.IsNullOrEmpty(HttpContext.Current?.User?.Identity?.Name)
+                ? HttpContext.Current.User.Identity.Name
+                : "Anonymous";
+
+            foreach (var entity in entities)
+            {
+                if (entity.State == EntityState.Added)
+                {
+                    ((BaseEntity)entity.Entity).DateCreated = DateTime.UtcNow;
+                    ((BaseEntity)entity.Entity).UserCreated = currentUsername;
+                }
+
+                ((BaseEntity)entity.Entity).DateModified = DateTime.UtcNow;
+                ((BaseEntity)entity.Entity).UserModified = currentUsername;
+            }
         }
     }
 }
---
+++ TreatTracker.Data/Treat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+
+namespace TreatTracker.Data
+{
+    public abstract class Treat : BaseEntity
+    {
+        [Required]
+        public string TreatName { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string SecretIngredient { get; set; }
+        [Required]
+        public decimal? Price { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
