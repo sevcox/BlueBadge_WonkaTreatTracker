fdc5806232823474af33ba256b3ba0900a8b48fe
tylerklink75 <66278928+tylerklink75@users.noreply.github.com>
2020-07-31 10:36:44 -0400

    first commit

---
+++ TreatTracker.Data/Character.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreatTracker.Data
+{
+    public class Character
+    {
+        [Key]
+        public int CharacterId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Weakness { get; set; }
+        [ForeignKey(nameof(RoomId))]
+        public virtual Room Room { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Age
+        {
+            get
+            {
+                if (DateOfBirth == new DateTime())
+                {
+                    return 9001;
+                }
+                TimeSpan ageSpan = DateTime.Now - DateOfBirth;
+                double totalAgeInYear = ageSpan.TotalDays / 365.25;
+                return Convert.ToInt32(Math.Floor(totalAgeInYear));
+            }
+        }
+
+
+
+
+    }
+}
