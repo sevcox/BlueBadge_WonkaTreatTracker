ecb4b9bfe14b5c091e2a6a12a8a3a3f0e30cf772
Severa <severa.cox@gmail.com>
2020-08-12 18:30:31 -0400

    final commit

--- TreatTracker.Services/FactoryService.cs
+++ TreatTracker.Services/FactoryService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TreatTracker.Data;
 using TreatTracker.Models;
+using TreatTracker.Models.CandyModels;
 
 namespace TreatTracker.Services
 {
@@ -53,6 +54,46 @@ namespace TreatTracker.Services
                     PhoneNumber = entity.PhoneNumber,
                     Manager = entity.Manager
                 };
+            }
+        }
+        public FactoryDetail GetFactoryByCandyId(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var factory =
+                    ctx
+                    .Candies
+                    .Single(e => e.CandyId == id)
+                    .Factory;
+                return
+                    new FactoryDetail
+                    {
+                        FactoryId = factory.FactoryId,
+                        LocationName = factory.LocationName,
+                        Address = factory.Address,
+                        Manager = factory.Manager,
+                        PhoneNumber = factory.PhoneNumber
+                    };
+            }
+        }
+        public FactoryDetail GetFactoryByDrinkId(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var factory =
+                    ctx
+                    .Drinks
+                    .Single(e => e.DrinkId == id)
+                    .Factory;
+                return
+                    new FactoryDetail
+                    {
+                        FactoryId = factory.FactoryId,
+                        LocationName = factory.LocationName,
+                        Address = factory.Address,
+                        Manager = factory.Manager,
+                        PhoneNumber = factory.PhoneNumber
+                    };
             }
         }
     }
